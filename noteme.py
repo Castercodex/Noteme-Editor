fbe467d243421473fc4fb22f2a1cae73a74a7e3c
Castercodex <edwardberth1@gmail.com>
2020-08-31 04:57:11 +0100

    Version1.01

--- noteme.py
+++ noteme.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog, Text, messagebox
 import os
 
 root = tk.Tk()
-note = []
 file_name =  None
 
+#functions
 def saveasfile(event=None):
     input_file_name = tk.filedialog.asksaveasfilename(initialdir='/', title='Save File', filetypes=(
     ('Text Documents', '*.txt'), ('All files', '*.*')))
@@ -24,6 +24,8 @@ def savefile(event=None):
         write_to_file(file_name)
     return "break"
 
+def on_change_text(event=None):
+    update_line_numbers()
 
 def openfile(event=None):
     input_file_name = filedialog.askopenfilename(initialdir='/', title='Open File', filetypes=(('Text Documents', '*.txt'),('All files', '*.*')))
@@ -34,6 +36,7 @@ def openfile(event=None):
         textfield.delete(1.0, 'end')
     with open(file_name) as _file:
         textfield.insert(1.0, _file.read())
+        on_change_text()
 
 def newfile(event=None):
     root.title("Untitled")
@@ -49,6 +52,29 @@ def write_to_file(file_name):
             the_file.write(content)
     except IOError:
         pass
+
+def get_line_numbers():
+    output = ''
+    row, col = textfield.index("end").split('.')
+    for i in range(1, int(row)):
+        output += str(i)+ '\n'
+    return output
+
+def update_line_numbers(event = None):
+    line_numbers = get_line_numbers()
+    lineNumberBar.config(state='normal')
+    lineNumberBar.delete('1.0', 'end')
+    lineNumberBar.insert('1.0', line_numbers)
+    lineNumberBar.config(state='disabled')
+
+def exit(event=None):
+    global file_name
+    if file_name:
+        root.destroy()
+    else:
+        tk.messagebox.showwarning('Warning', 'Content Not Saved')
+        savefile()
+
 
 
 #THE MENUBAR
@@ -72,31 +98,40 @@ fileMenu.add_command(label="Save", accelerator='Ctrl + S', compound='left', comm
 fileMenu.add_command(label="Save as", accelerator='Ctrl + Shift + S', compound='left', command=saveasfile)
 root.config(menu=menuBar)
 
+#frame
 frame = tk.Frame(root,bg='#000')
 frame.pack(fill='x', ipady=10)
-lineNumberBar = tk.Text(root, width=6, padx=3,pady=300, takefocus=0, border=0, background='#cbc6c6', state='disabled')
+lineNumberBar = tk.Text(root, width=6,  takefocus=0, border=0, background='#cbc6c6', state='disabled')
 lineNumberBar.pack(side='left', fill='y')
+
+#Buttons
 newfileBtn = tk.Button(frame, text='New File', bg="#4dec4d", height=2, width=10, borderwidth=0, command=newfile)
 newfileBtn.pack(side='left')
 savefileBtn = tk.Button(frame, text='Save File', bg='#00a6ff',height=2, width=10, borderwidth=0, command=savefile)
 savefileBtn.pack(side='left', padx=10)
 openfileBtn = tk.Button(frame, text='Open File', bg='#ecc64d',height=2, width=10, borderwidth=0, command=openfile)
 openfileBtn.pack(side='left')
+#Textarea
 textfield = tk.Text(root, bg='#fff', fg='#000', wrap='word', undo=1)
 textfield.pack(expand='yes', fill='both')
+
+#Scrollbars
 scrollBar = tk.Scrollbar(textfield)
 textfield.configure(yscrollcommand=scrollBar.set)
 scrollBar.config(command=textfield.yview)
 scrollBar.pack(side='right', fill='y')
+lineNumberBar.configure(yscrollcommand=scrollBar.set)
+#Binding the keyboard shortcuts to function
 textfield.bind('<Control-s>', savefile)
 textfield.bind('<Control-o>', openfile)
 textfield.bind('<Control-n>', newfile)
 textfield.bind('<Control-Shift-s>', saveasfile)
 textfield.bind('<Control-Shift-S>', saveasfile)
+textfield.bind('<Any-KeyPress>', on_change_text)
 
 
 
-
+root.protocol('WM_DELETE_WINDOW', exit)
 root.title("NoteME")
 root.minsize(1000,500)
 root.mainloop()
